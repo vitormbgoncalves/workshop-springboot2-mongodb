8ccb3ab3b2b8203e78bf04dc7f6098c16e3f5461
Vitor Gonçalves <vitor@innovativetecnologia.com.br>
2020-10-14 19:36:39 -0300

    Consulta simples com @Query

--- src/main/java/com/innovativetecnologia/springboot2mongodb/repository/PostRepository.java
+++ src/main/java/com/innovativetecnologia/springboot2mongodb/repository/PostRepository.java
@@ -1,14 +1,17 @@
 package com.innovativetecnologia.springboot2mongodb.repository;
 
 import com.innovativetecnologia.springboot2mongodb.domain.Post;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ <'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 }
