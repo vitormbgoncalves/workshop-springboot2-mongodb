b5967917b0c729e7f4d6b8b7d6deea6d2200a024
Vitor Gonçalves <vitor@innovativetecnologia.com.br>
2020-10-14 18:28:06 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/innovativetecnologia/springboot2mongodb/config/Instantiation.java
+++ src/main/java/com/innovativetecnologia/springboot2mongodb/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.innovativetecnologia.springboot2mongodb.config;
 import com.innovativetecnologia.springboot2mongodb.domain.Post;
 import com.innovativetecnologia.springboot2mongodb.domain.User;
 import com.innovativetecnologia.springboot2mongodb.dto.AuthorDTO;
+import com.innovativetecnologia.springboot2mongodb.dto.CommentDTO;
 import com.innovativetecnologia.springboot2mongodb.repository.PostRepository;
 import com.innovativetecnologia.springboot2mongodb.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -39,6 +40,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post pos1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
         Post pos2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei felix hoje!", new AuthorDTO(maria));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+
+        pos1.getComments().addAll(Arrays.asList(c1, c2));
+        pos2.getComments().addAll(Arrays.asList(c3));
 
         postRepository.saveAll(Arrays.asList(pos1, pos2));
 
