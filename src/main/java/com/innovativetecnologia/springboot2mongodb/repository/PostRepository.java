c39a8ee2295a486a0f8ec40ae7ce6de92597525e
Vitor Gonçalves <vitor@innovativetecnologia.com.br>
2020-10-14 19:08:08 -0300

    Consulta simples com query methods

--- src/main/java/com/innovativetecnologia/springboot2mongodb/repository/PostRepository.java
+++ src/main/java/com/innovativetecnologia/springboot2mongodb/repository/PostRepository.java
@@ -1,10 +1,14 @@
 package com.innovativetecnologia.springboot2mongodb.repository;
 
 import com.innovativetecnologia.springboot2mongodb.domain.Post;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/innovativetecnologia/springboot2mongodb/services/PostService.java
+++ src/main/java/com/innovativetecnologia/springboot2mongodb/services/PostService.java
@@ -6,6 +6,7 @@ import com.innovativetecnologia.springboot2mongodb.services.exception.ObjectNotF
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -17,5 +18,9 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public List<Post> findByTitle(String text) {
+        return repo.findByTitleContainingIgnoreCase(text);
     }
 }
