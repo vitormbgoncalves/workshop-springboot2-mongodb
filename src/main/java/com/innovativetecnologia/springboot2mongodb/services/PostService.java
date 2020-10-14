b4885ea85fed0022b47a327e61e3232d98916373
Vitor Gonçalves <vitor@innovativetecnologia.com.br>
2020-10-14 18:15:30 -0300

    Obtendo um post por id

---
+++ src/main/java/com/innovativetecnologia/springboot2mongodb/services/PostService.java
@@ -0,0 +1,21 @@
+package com.innovativetecnologia.springboot2mongodb.services;
+
+import com.innovativetecnologia.springboot2mongodb.domain.Post;
+import com.innovativetecnologia.springboot2mongodb.repository.PostRepository;
+import com.innovativetecnologia.springboot2mongodb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
