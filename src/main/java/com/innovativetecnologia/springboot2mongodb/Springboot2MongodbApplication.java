3b71fdea7290a02a2a6fdd08bc3a10f7bb9f80c7
Vitor Gonçalves <vitor@innovativetecnologia.com.br>
2020-10-14 11:42:07 -0300

    Initial commit

---
+++ src/main/java/com/innovativetecnologia/springboot2mongodb/Springboot2MongodbApplication.java
@@ -0,0 +1,13 @@
+package com.innovativetecnologia.springboot2mongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2MongodbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2MongodbApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/innovativetecnologia/springboot2mongodb/Springboot2MongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innovativetecnologia.springboot2mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot2MongodbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
